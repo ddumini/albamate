0d617563a4a677ec96187bdcd0c79be75c2ce311
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-11 02:56:51 +0900

    ✨Feat: 다크모드 적용을 위한 토글버튼 컴포넌트 구현

---
+++ src/shared/components/ThemeToggle.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  const isDark = theme === "dark";
+
+  return (
+    <button
+      aria-label="Toggle Dark Mode"
+      className="relative flex items-center justify-center w-40 h-40 rounded-full bg-white transition-colors duration-500 dark-light-toggle"
+      type="button"
+      onClick={() => setTheme(isDark ? "light" : "dark")}
+    >
+      {/* Sun Icon */}
+      <svg
+        className={`w-20 h-20 text-black absolute transition-opacity duration-500 ease-in-out ${
+          isDark ? "opacity-0" : "opacity-100"
+        }`}
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <circle cx="12" cy="12" r="5" />
+        <g stroke="currentColor" strokeWidth="2">
+          <line x1="12" x2="12" y1="1" y2="4" />
+          <line x1="12" x2="12" y1="20" y2="23" />
+          <line x1="4.22" x2="6.34" y1="4.22" y2="6.34" />
+          <line x1="17.66" x2="19.78" y1="17.66" y2="19.78" />
+          <line x1="1" x2="4" y1="12" y2="12" />
+          <line x1="20" x2="23" y1="12" y2="12" />
+          <line x1="4.22" x2="6.34" y1="19.78" y2="17.66" />
+          <line x1="17.66" x2="19.78" y1="6.34" y2="4.22" />
+        </g>
+      </svg>
+
+      {/* Moon Icon */}
+      <svg
+        className={`w-20 h-20 text-black absolute transition-opacity duration-500 ease-in-out transform ${
+          isDark
+            ? "opacity-100 scale-120 translate-x-[-2px] translate-y-[1px]"
+            : "opacity-0 scale-90"
+        }`}
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path d="M21 12.79A9 9 0 0 1 11.21 3 7 7 0 1 0 21 12.79z" />
+      </svg>
+    </button>
+  );
+};
+
+export default ThemeToggle;
