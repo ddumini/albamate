c3139ee14cd1c30b6dcb2b0553dd457f43d9a6bc
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-11 02:20:07 +0900

    ✨Feat: SSR과 클라이언트 측 하이드레이션 과정 경고 무시를 위한 속성 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-import './globals.css';
+import "./globals.css";
 
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 
-import { Providers } from './providers';
+import { Providers } from "./providers";
 
 const pretendard = localFont({
-  src: '../../public/fonts/PretendardVariable.woff2',
-  display: 'swap',
-  variable: '--font-pretendard',
-  weight: '100 900',
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  variable: "--font-pretendard",
+  weight: "100 900",
 });
 
 const RootLayout = ({
@@ -17,7 +17,7 @@ const RootLayout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <html lang="ko">
+    <html suppressHydrationWarning lang="ko">
       <body className={pretendard.className}>
         <Providers>{children}</Providers>
       </body>
