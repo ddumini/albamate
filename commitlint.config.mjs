cecf1070f93f38ca3cf385b40f7b1a93e27a612b
sumin <tnals2565@naver.com>
2025-07-10 14:43:44 +0900

    📦️ Chore: gitmoji test
    gitmoji test

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,27 +1,11 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat', // 새로운 기능
-        'fix', // 버그 수정
-        'docs', // 문서 변경
-        'style', // 코드 스타일 변경
-        'refactor', // 리팩토링
-        'perf', // 성능 개선
-        'test', // 테스트 추가/수정
-        'chore', // 기타 변경사항
-        'ci', // CI 설정 변경
-        'build', // 빌드 시스템 변경
-        'revert', // 이전 커밋 되돌리기
-      ],
-    ],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
+    'type-enum': [0], // type-enum 규칙 비활성화
+    'type-case': [0], // type-case 규칙 비활성화
+    'type-empty': [0], // type-empty 규칙 비활성화
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-empty': [2, 'never'],
+    'subject-empty': [0], // subject-empty 규칙 비활성화
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 72],
   },
