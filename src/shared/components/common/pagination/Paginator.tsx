0330708e023c23c183c80cc00360d44bcb60cf4b
Dong-Han-Kim <dh32110@gmail.com>
2025-07-11 13:29:35 +0900

    🐛Fix : Paginator 변수명 변경 및 스타일 수정

--- src/shared/components/common/pagination/Paginator.tsx
+++ src/shared/components/common/pagination/Paginator.tsx
@@ -10,8 +10,8 @@ interface PaginatorProps {
 
 const Paginator = ({ currentPage, totalPages, onChange }: PaginatorProps) => {
   const width = useWindowWidth();
-  const isSmallScreen = typeof width === "number" && width < 1024;
-  const pageThreshold = isSmallScreen ? 3 : 5;
+  const isSmallScreen = typeof width === "number" && width < 769;
+  const visiblePages = isSmallScreen ? 3 : 5;
 
   const renderPageNumbers = () => {
     const pages: (string | number)[] = [];
@@ -24,23 +24,23 @@ const Paginator = ({ currentPage, totalPages, onChange }: PaginatorProps) => {
       return range;
     };
 
-    if (totalPages <= pageThreshold) {
+    if (totalPages <= visiblePages) {
       // 전체 페이지 수가 적으면 모두 표시
       pages.push(...createRange(1, totalPages));
     } else {
-      const showLeft = currentPage <= pageThreshold - 2;
-      const showRight = currentPage >= totalPages - (pageThreshold - 2);
+      const showLeft = currentPage <= visiblePages - 1;
+      const showRight = currentPage >= totalPages - (visiblePages - 2);
       if (showLeft) {
         // 앞쪽 페이지 구간
-        pages.push(...createRange(1, pageThreshold));
+        pages.push(...createRange(1, visiblePages));
         pages.push("...", totalPages);
       } else if (showRight) {
         // 뒤쪽 페이지 구간
         pages.push(1, "...");
-        pages.push(...createRange(totalPages - pageThreshold + 1, totalPages));
+        pages.push(...createRange(totalPages - visiblePages + 1, totalPages));
       } else {
         // 중간 페이지 구간
-        const sidePages = Math.floor((pageThreshold - 2) / 2); // 양쪽에 표시할 페이지 수
+        const sidePages = Math.floor((visiblePages - 2) / 2); // 양쪽에 표시할 페이지 수
         pages.push(1, "...");
         pages.push(
           ...createRange(currentPage - sidePages, currentPage + sidePages),
@@ -51,18 +51,20 @@ const Paginator = ({ currentPage, totalPages, onChange }: PaginatorProps) => {
 
     return pages.map((page, index) => {
       const pageClassName =
-        currentPage === page ? "text-black-400" : "text-gray-200";
+        currentPage === page
+          ? "text-black-400 font-semibold"
+          : "text-gray-200 font-medium";
       return page === "..." ? (
-        <span
-          key={`${index + "a"}`}
-          className="flex justify-center items-center w-34 h-34 pc:w-48 pc:h-48 text-md pc:text-2lg"
+        <div
+          key={`ellipsis-${index}`}
+          className="flex justify-center items-center w-34 h-34 pc:w-48 pc:h-48 text-md pc:text-2lg bg-background-200 rounded-md pc:rounded-lg text-gray-200 pb-1/3 font-medium"
         >
-          ...
-        </span>
+          <span className="h-fit leading-none pb-9">...</span>
+        </div>
       ) : (
         <button
-          key={`${index + "b"}`}
-          className={`flex justify-center items-center w-34 h-34 pc:w-48 pc:h-48 cursor-pointer text-center text-md pc:text-2lg ${pageClassName}`}
+          key={`page-${page}`}
+          className={`flex justify-center items-center w-34 h-34 pc:w-48 pc:h-48 cursor-pointer text-center text-md pc:text-2lg bg-background-200 rounded-md pc:rounded-lg ${pageClassName}`}
           disabled={currentPage === page}
           onClick={() => onChange(Number(page))}
         >
@@ -75,7 +77,7 @@ const Paginator = ({ currentPage, totalPages, onChange }: PaginatorProps) => {
   return (
     <div className="flex items-center gap-4">
       <button
-        className="w-34 h-34 pc:w-48 pc:h-48 bg-background-200 p-9 pc:p-12"
+        className="w-34 h-34 pc:w-48 pc:h-48 bg-background-200 p-9 pc:p-12 rounded-md pc:rounded-lg"
         disabled={currentPage === 1}
         onClick={() => onChange(currentPage - 1)}
       >
@@ -93,7 +95,7 @@ const Paginator = ({ currentPage, totalPages, onChange }: PaginatorProps) => {
       </button>
       {renderPageNumbers()}
       <button
-        className="w-34 h-34 pc:w-48 pc:h-48 bg-background-200 p-9 pc:p-12"
+        className="w-34 h-34 pc:w-48 pc:h-48 bg-background-200 p-9 pc:p-12 rounded-md pc:rounded-lg"
         disabled={currentPage === totalPages}
         onClick={() => onChange(currentPage + 1)}
       >
