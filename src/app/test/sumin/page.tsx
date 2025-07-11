30aac9228db5bc59287a016f4f9999782efc54b5
sumin <tnals2565@naver.com>
2025-07-11 16:52:04 +0900

    🐛 Refactor: dropdown 예시 페이지 변수 스코프 변경
    refactor: dropdown 예시 페이지 변수 스코프 변경

--- src/app/test/sumin/page.tsx
+++ src/app/test/sumin/page.tsx
@@ -3,20 +3,20 @@
 import InputDropdown from '@/shared/components/common/input-dropdown';
 import Select from '@/shared/components/common/select';
 
-const filterOptions = [
-  { value: 'latest', label: '전체' },
-  { value: 'popular', label: '공개' },
-  { value: 'oldest', label: '비공개' },
-];
-const sortOptions = [
-  { value: 'latest', label: '최신순' },
-  { value: 'high-wage', label: '시급높은순' },
-  { value: 'many-applicants', label: '지원자 많은순' },
-  { value: 'many-scraps', label: '스크랩 많은순' },
-];
-const inputOptions = [{ value: '전체' }, { value: '없음' }];
-
 const Sumin = () => {
+  const filterOptions = [
+    { value: 'latest', label: '전체' },
+    { value: 'popular', label: '공개' },
+    { value: 'oldest', label: '비공개' },
+  ];
+  const sortOptions = [
+    { value: 'latest', label: '최신순' },
+    { value: 'high-wage', label: '시급높은순' },
+    { value: 'many-applicants', label: '지원자 많은순' },
+    { value: 'many-scraps', label: '스크랩 많은순' },
+  ];
+  const inputOptions = [{ value: '전체' }, { value: '없음' }];
+
   const handleFilterChange = (filterId: string) => {
     console.log(filterId);
     // API 호출 또는 데이터 필터링 로직
