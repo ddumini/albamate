43692235dbefb44eaafbaea38bdcec2b8205d530
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-11 14:41:49 +0900

    🐛Fix: 배포 시 오류방지를 위한 절대경로 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
 
-import { Providers } from "./providers";
+import { Providers } from "@/app/providers";
 
 const pretendard = localFont({
   src: "../../public/fonts/PretendardVariable.woff2",
