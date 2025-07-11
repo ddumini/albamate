f3b40ad64816610b8d258e39d5f65c606e0adc36
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-11 13:48:31 +0900

    🐛Fix: layout의 번들 사이즈 증가를 방지하기 위해 코드 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import "./globals.css";
 
 import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
-import { useEffect, useState } from "react";
 
 import { Providers } from "./providers";
 
@@ -20,21 +17,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    // hydration 오류 방지: SSR 중엔 아무것도 안 보여줌
-    return (
-      <html lang="ko">
-        <body />
-      </html>
-    );
-  }
-
   return (
     <html lang="ko">
       <body className={pretendard.className}>
