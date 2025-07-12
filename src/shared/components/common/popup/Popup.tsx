e5192400fae23b8bb37f07ff113b736cfd66a9cb
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 04:43:14 +0900

    ✨Feat: 팝업 창 닫기 기능 구현 및 세부 반응형 적용

--- src/shared/components/common/popup/Popup.tsx
+++ src/shared/components/common/popup/Popup.tsx
@@ -12,9 +12,9 @@ const Popup = ({ message }: ToastLikePopupProps) => {
   if (!visible) return null;
 
   return (
-    <div className="BG-blueblack Text-white-gray fixed top-8 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-6 text-xs shadow-lg md:px-40 md:py-12 md:text-md">
+    <div className="BG-blueblack Text-white-gray fixed top-8 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap shadow-lg md:px-40 md:text-md lg:px-80 lg:py-20 lg:text-lg">
       {/* 아이콘 */}
-      <div className="relative h-24 w-24">
+      <div className="relative h-24 w-24 lg:h-36 lg:w-36">
         <Image fill alt="사용자 이미지" src="/icons/user.svg" />
       </div>
 
@@ -23,11 +23,11 @@ const Popup = ({ message }: ToastLikePopupProps) => {
 
       {/* 닫기 버튼 */}
       <button
-        aria-label="닫기"
-        className="text-white-gray transition-colors hover:text-white"
+        className="relative h-24 w-24 cursor-pointer md:h-30 md:w-30"
+        type="button"
         onClick={() => setVisible(false)}
       >
-        ✕
+        <Image fill alt="닫기 버튼" src="/icons/x-thin.svg" />
       </button>
     </div>
   );
