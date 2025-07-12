8e5274c2ccc900a3f6edd326b0b600e207e4ac07
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 06:05:00 +0900

    ✨Feat: 수정 팝업 창 버튼 클릭을 통한 트리거 및 자동 사라짐 구현

--- src/shared/components/common/popup/EditPopup.tsx
+++ src/shared/components/common/popup/EditPopup.tsx
@@ -1,24 +1,40 @@
 'use client';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface PopupProps {
   message: string;
+  duration?: number;
+  visible: boolean;
+  onClose: () => void;
 }
 
-const EditPopup = ({ message }: PopupProps) => {
-  const [visible, setVisible] = useState(true);
+const EditPopup = ({
+  message,
+  duration = 3000,
+  visible,
+  onClose,
+}: PopupProps) => {
+  const [shouldRender, setShouldRender] = useState(visible);
 
-  if (!visible) return null;
+  useEffect(() => {
+    if (visible) {
+      setShouldRender(true);
+      const timer = setTimeout(() => {
+        setShouldRender(false);
+        onClose();
+      }, duration);
+      return () => clearTimeout(timer);
+    }
+  }, [visible, duration, onClose]);
+
+  if (!shouldRender) return null;
 
   return (
     <div className="BG-blueblack Text-white-gray fixed top-60 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-80 py-12 text-xs whitespace-nowrap opacity-95 shadow-lg md:px-120 md:text-md lg:px-150 lg:text-lg">
-      {/* 아이콘 */}
       <div className="relative h-24 w-24 lg:h-36 lg:w-36">
         <Image fill alt="정보 수정 이미지" src="/icons/check-circle.svg" />
       </div>
-
-      {/* 메시지 */}
       <span className="flex-1">{message}</span>
     </div>
   );
