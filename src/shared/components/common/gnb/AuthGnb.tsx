2634c825b2dd19f618cddf913be99c31d99c9871
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-12 15:31:47 +0900

    💄Style: 로그인 전 헤더 반응형 적용

--- src/shared/components/common/gnb/AuthGnb.tsx
+++ src/shared/components/common/gnb/AuthGnb.tsx
@@ -14,32 +14,34 @@ const AuthGnb = () => {
   if (!mounted) return null;
 
   return (
-    <header className="w-full border-b border-gray-200 px-12 md:px-32 dark:border-gray-500">
+    <header className="w-full border-b border-gray-200 md:px-16 md:px-72 lg:px-120 dark:border-gray-500">
       <div className="mx-auto flex max-w-7xl items-center justify-center px-4 md:justify-between md:py-3">
         {/* 좌측 영역 */}
         <div className="flex items-center md:gap-24">
           {/* 로고 */}
           <button
-            className="flex items-center space-x-2 text-xl font-bold"
+            className="flex cursor-pointer items-center space-x-2 py-15 text-xl font-bold md:py-24"
+            type="button"
             onClick={() => router.push('/')}
           >
-            <div className="relative mr-8 h-16 w-16 md:mr-6 md:h-24 md:w-24">
+            <div className="relative mr-8 h-16 w-16 md:mr-6 md:h-24 md:w-24 lg:h-36 lg:w-36">
               <Image fill alt="로고 이미지" src="/images/logo.svg" />
             </div>
-            <div className="relative h-24 w-70 md:h-40 md:w-100">
+            <div className="relative h-24 w-124 lg:h-36 lg:w-212">
               <Image fill alt="로고 명" src="/images/logo-typo.svg" />
             </div>
           </button>
         </div>
 
         {/* 우측 영역 */}
-        <div className="hidden items-center gap-4 md:flex">
+        <div className="hidden items-center gap-24 md:flex">
           {/* 네비게이션 메뉴 */}
-          <nav className="mr-16 flex gap-12 text-xs font-medium md:text-xs lg:text-sm">
+          <nav className="text-16 mr-16 flex gap-24 font-medium md:text-xs lg:text-sm lg:text-[20px]">
             <button
               className={`hover:text-primary cursor-pointer border-none bg-transparent p-0 text-inherit transition-colors ${
                 pathname === '/owner' ? 'text-mint-100' : ''
               }`}
+              type="button"
               onClick={() => router.push('/owner')}
             >
               사장님 전용
@@ -48,6 +50,7 @@ const AuthGnb = () => {
               className={`hover:text-primary cursor-pointer border-none bg-transparent p-0 text-inherit transition-colors ${
                 pathname === '/worker' ? 'text-mint-100' : ''
               }`}
+              type="button"
               onClick={() => router.push('/worker')}
             >
               지원자 전용
