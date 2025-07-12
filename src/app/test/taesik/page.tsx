81f84780276217b4fd58fb0e86ee0367b8135e4f
leetaesik <vlwkvkd@naver.com>
2025-07-12 14:34:10 +0900

    ✨Feat: 테스트

--- src/app/test/taesik/page.tsx
+++ src/app/test/taesik/page.tsx
@@ -1,10 +1,30 @@
+'use client';
+
+import { useRef } from 'react';
+
+import Icon from '@/shared/components/common/input/Icon';
+import Input from '@/shared/components/common/input/Input';
 import Profile from '@/shared/components/common/profile/Profile';
 import ProfileEdit from '@/shared/components/common/profile/ProfileEdit';
 
 const TaesikTest = () => {
+  const inputRef1 = useRef<HTMLInputElement>(null);
   return (
     <div className="bg-background-100">
       <div>태식 테스트 페이지</div>
+      <Icon alt="위치" src="/icons/pin-solid.svg" />
+      <Icon
+        alt="위치"
+        src="/icons/pin-solid.svg"
+        onClick={() => alert('클릭')}
+      />
+      <div className="flex gap-10 p-10">
+        <Input ref={inputRef1} name="input1" />
+        <Input variant="outlined" />
+        <Input isInvalid />
+        <Input isInvalid variant="outlined" />
+        <button onClick={() => inputRef1.current?.focus()}>ref 테스트</button>
+      </div>
       <Profile imageUrl="https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Albaform/user/415/1752142046479/15-2_5882_1752141901515.jpeg" />
       <Profile />
       <Profile className="size-26 border-none lg:size-26" sizes="26px" />
