7aa99df7c808b05f655fdf6e78aef19b63187c43
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 07:08:02 +0900

    ✨Feat: JSDoc 작성 및 문구 수정

--- src/shared/components/common/popup/Popup.tsx
+++ src/shared/components/common/popup/Popup.tsx
@@ -2,6 +2,13 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
+/**
+ * @typedef {Object} ToastLikePopupProps
+ * @property {number} [count] - 현재 지원한 알바생 수
+ * @property {number} [duration=3000] - 자동으로 닫히기까지의 시간 (ms)
+ * @property {boolean} visible - 팝업 표시 여부
+ * @property {() => void} onClose - 팝업 종료 시 실행되는 콜백 함수
+ */
 interface ToastLikePopupProps {
   count?: number;
   duration?: number;
@@ -9,6 +16,13 @@ interface ToastLikePopupProps {
   onClose: () => void;
 }
 
+/**
+ * 현재 이 페이지를 보고 있는 사용자 수를 나타내는 토스트 형태의 팝업 컴포넌트입니다.
+ * 자동 닫힘 기능과 애니메이션 효과를 포함합니다.
+ *
+ * @component
+ * @param {ToastLikePopupProps} props - 팝업에 전달되는 props
+ */
 const Popup = ({
   count = 3,
   duration = 3000,
@@ -45,7 +59,7 @@ const Popup = ({
 
   return (
     <div
-      className={`BG-blueblack Text-white-gray fixed top-50 left-1/2 z-50 mx-12 flex max-w-[1200px] min-w-[300px] -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap shadow-lg transition-all duration-500 ease-in-out md:px-80 md:text-md lg:px-[300px] lg:text-lg ${animationClass} `}
+      className={`BG-blueblack Text-white-gray fixed top-50 left-1/2 z-50 mx-12 flex max-w-[1200px] min-w-[300px] -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap shadow-lg transition-all duration-500 ease-in-out md:px-80 md:text-md lg:px-300 lg:text-lg ${animationClass} `}
       style={{ width: 'calc(100vw - 3rem * 2)' }}
     >
       {/* 아이콘 */}
@@ -55,8 +69,8 @@ const Popup = ({
 
       {/* 메시지 */}
       <span className="flex-1">
-        현재 <span className="font-semibold text-mint-400">{count}명</span>이 이
-        알바폼을 보는 중이에요!
+        현재 <span className="font-semibold text-mint-400">{count}명</span>이
+        지원했어요!
       </span>
 
       {/* 닫기 버튼 */}
