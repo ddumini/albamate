83f507fbbf6366938aa94b836fbdc3ca456dfd66
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 04:54:17 +0900

    ✨Feat: 시간이 지나면 자동으로 사라지는 기능 구현

--- src/shared/components/common/popup/Popup.tsx
+++ src/shared/components/common/popup/Popup.tsx
@@ -1,28 +1,35 @@
 'use client';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface ToastLikePopupProps {
   count?: number; // 숫자 동적 처리용 (선택)
+  duration?: number; // 토스트 보여줄 초 단위
 }
 
-const Popup = ({ count }: ToastLikePopupProps) => {
+const Popup = ({ count = 3, duration = 3000 }: ToastLikePopupProps) => {
   const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    if (!visible) return;
+
+    const timer = setTimeout(() => setVisible(false), duration);
+    return () => clearTimeout(timer);
+  }, [visible, duration]);
 
   if (!visible) return null;
 
   return (
-    <div className="BG-blueblack Text-white-gray fixed top-600 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap shadow-lg md:px-40 md:text-md lg:px-80 lg:text-lg">
+    <div className="BG-blueblack Text-white-gray fixed top-24 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap shadow-lg md:px-40 md:text-md lg:px-80 lg:text-lg">
       {/* 아이콘 */}
       <div className="relative h-24 w-24 lg:h-36 lg:w-36">
         <Image fill alt="사용자 이미지" src="/icons/user.svg" />
       </div>
 
-      {/* 메시지: count가 있으면 강조용 span으로 분리 */}
+      {/* 메시지 */}
       <span className="flex-1">
-        현재{' '}
-        <span className="font-semibold text-mint-400">{count ?? '3'}명</span>이
-        이 알바폼을 보는 중이에요!
+        현재 <span className="font-semibold text-mint-400">{count}명</span>이 이
+        알바폼을 보는 중이에요!
       </span>
 
       {/* 닫기 버튼 */}
