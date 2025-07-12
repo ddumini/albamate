6dc9113dd1a4126880a1e9c7cdf74f33e2828505
hizdev <gpfksdlrn@naver.com>
2025-07-13 06:11:32 +0900

    ✨ Feat: FloatingButton 컴포넌트 추가

--- src/app/test/hyeran/HyeranClientTest.tsx
+++ src/app/test/hyeran/HyeranClientTest.tsx
@@ -3,9 +3,12 @@
 import { useState } from 'react';
 
 import Checkbox from '@/shared/components/common/button/Checkbox';
+import FloatingButton from '@/shared/components/common/button/FloatingButton';
+import FloatingButtonContainer from '@/shared/components/common/button/FloatingButtonContainer';
 import RadioButton, {
   RadioOption,
 } from '@/shared/components/common/button/RadioButton';
+import ThemeToggle from '@/shared/components/ThemeToggle';
 
 const RADIO_OPTIONS: RadioOption[] = [
   { value: 'REJECTED', label: '거절' },
@@ -23,8 +26,21 @@ const HyeranClientTest = () => {
   const [checked3, setChecked3] = useState(false);
   const [checked4, setChecked4] = useState(true);
 
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const handleBookmarkToggle = () => {
+    const newBookmarkState = !isBookmarked;
+    if (isBookmarked) {
+      // 북마크 제거 API 호출 추후 구현
+    } else {
+      // 북마크 추가 API 호출 추후 구현
+    }
+    setIsBookmarked(newBookmarkState);
+  };
+
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-20">
+      <ThemeToggle />
       <section>
         <h1>라디오 버튼 컴포넌트</h1>
         <RadioButton
@@ -63,6 +79,32 @@ const HyeranClientTest = () => {
           label="네 번째 옵션"
           onChange={setChecked4}
         />
+      </section>
+      <section>
+        <h1>FloatingButton</h1>
+        <FloatingButton href="/forms/create" text="폼 만들기" type="addForm" />
+        <FloatingButton href="/albatalk/create" type="addAlbatalk" />
+        <FloatingButton
+          isBookmarked={isBookmarked}
+          type="bookmark"
+          onClick={handleBookmarkToggle}
+        />
+        <FloatingButton type="share" />
+        <FloatingButtonContainer position="right-center">
+          <FloatingButton
+            isBookmarked={isBookmarked}
+            type="bookmark"
+            onClick={handleBookmarkToggle}
+          />
+          <FloatingButton type="share" />
+        </FloatingButtonContainer>
+        <FloatingButtonContainer>
+          <FloatingButton
+            href="/forms/create"
+            text="폼 만들기"
+            type="addForm"
+          />
+        </FloatingButtonContainer>
       </section>
     </div>
   );
