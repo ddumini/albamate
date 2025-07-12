50ca0dea48f29e355ff3a5f5371bd6eb6cf22c04
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 05:44:22 +0900

    ✨Feat: trainsition을 통한 부드러운 연출과 fadein을 통한 자연스러운 사라짐 적용

--- src/shared/components/common/popup/Popup.tsx
+++ src/shared/components/common/popup/Popup.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface ToastLikePopupProps {
   count?: number;
@@ -15,19 +15,31 @@ const Popup = ({
   visible,
   onClose,
 }: ToastLikePopupProps) => {
+  const [shouldRender, setShouldRender] = useState(visible);
+
+  // mount / unmount 처리 (애니메이션 포함)
+  useEffect(() => {
+    if (visible) {
+      setShouldRender(true);
+    } else {
+      const timer = setTimeout(() => setShouldRender(false), 300); // transition duration
+      return () => clearTimeout(timer);
+    }
+  }, [visible]);
+
+  // 자동 닫힘 처리
   useEffect(() => {
     if (!visible) return;
-
     const timer = setTimeout(() => onClose(), duration);
     return () => clearTimeout(timer);
   }, [visible, duration, onClose]);
 
-  if (!visible) return null;
+  if (!shouldRender) return null;
 
   return (
     <div
-      className="BG-blueblack Text-white-gray fixed top-50 left-1/2 z-50 mx-12 flex max-w-[1200px] -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap opacity-95 shadow-lg md:px-80 md:text-md lg:px-[300px] lg:text-lg"
-      style={{ width: 'calc(100vw - 3rem * 2)' }} // 3rem = 12 (mx-12)
+      className={`BG-blueblack Text-white-gray fixed top-50 left-1/2 z-50 mx-12 flex max-w-[1200px] -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap opacity-95 shadow-lg transition-all duration-300 ease-in-out md:px-80 md:text-md lg:px-[300px] lg:text-lg ${visible ? 'translate-y-0 opacity-100' : '-translate-y-4 opacity-0'} `}
+      style={{ width: 'calc(100vw - 3rem * 2)' }} // 3rem = mx-12
     >
       {/* 아이콘 */}
       <div className="relative h-24 w-24 lg:h-36 lg:w-36">
