ce741c28593d87646f73467de9f4c530c11e8c61
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 04:46:15 +0900

    ✨Feat: 메시지 prop 삭제와 접근성 추가및 호버 스타일 적용

--- src/shared/components/common/popup/Popup.tsx
+++ src/shared/components/common/popup/Popup.tsx
@@ -3,27 +3,32 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 interface ToastLikePopupProps {
-  message: string;
+  count?: number; // 숫자 동적 처리용 (선택)
 }
 
-const Popup = ({ message }: ToastLikePopupProps) => {
+const Popup = ({ count }: ToastLikePopupProps) => {
   const [visible, setVisible] = useState(true);
 
   if (!visible) return null;
 
   return (
-    <div className="BG-blueblack Text-white-gray fixed top-8 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap shadow-lg md:px-40 md:text-md lg:px-80 lg:py-20 lg:text-lg">
+    <div className="BG-blueblack Text-white-gray fixed top-600 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap shadow-lg md:px-40 md:text-md lg:px-80 lg:text-lg">
       {/* 아이콘 */}
       <div className="relative h-24 w-24 lg:h-36 lg:w-36">
         <Image fill alt="사용자 이미지" src="/icons/user.svg" />
       </div>
 
-      {/* 메시지 */}
-      <span className="flex-1">{message}</span>
+      {/* 메시지: count가 있으면 강조용 span으로 분리 */}
+      <span className="flex-1">
+        현재{' '}
+        <span className="font-semibold text-mint-400">{count ?? '3'}명</span>이
+        이 알바폼을 보는 중이에요!
+      </span>
 
       {/* 닫기 버튼 */}
       <button
-        className="relative h-24 w-24 cursor-pointer md:h-30 md:w-30"
+        aria-label="닫기"
+        className="relative h-24 w-24 cursor-pointer transition hover:brightness-75 md:h-30 md:w-30 lg:h-36 lg:w-36"
         type="button"
         onClick={() => setVisible(false)}
       >
