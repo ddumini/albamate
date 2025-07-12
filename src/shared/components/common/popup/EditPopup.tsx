e8add1bb0da378bcbb1977771c4410a9dfbe30b4
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 06:20:29 +0900

    ✨Feat: 성공, 실패, 정보에 따라 다른 팝업 창 표시를 위한 모듈화

--- src/shared/components/common/popup/EditPopup.tsx
+++ src/shared/components/common/popup/EditPopup.tsx
@@ -2,19 +2,29 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
-interface PopupProps {
+interface EditPopupProps {
   message: string;
   duration?: number;
   visible: boolean;
   onClose: () => void;
+  type?: 'success' | 'error' | 'info'; // 메시지 타입
+  iconSrc?: string; // 아이콘 직접 전달 가능
 }
+
+const defaultIcons = {
+  success: '/icons/check-circle.svg',
+  error: '/icons/X-circle.svg',
+  info: '/icons/info.svg',
+};
 
 const EditPopup = ({
   message,
   duration = 3000,
   visible,
   onClose,
-}: PopupProps) => {
+  type = 'success',
+  iconSrc,
+}: EditPopupProps) => {
   const [shouldRender, setShouldRender] = useState(visible);
 
   useEffect(() => {
@@ -33,7 +43,11 @@ const EditPopup = ({
   return (
     <div className="BG-blueblack Text-white-gray fixed top-60 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-80 py-12 text-xs whitespace-nowrap opacity-95 shadow-lg md:px-120 md:text-md lg:px-150 lg:text-lg">
       <div className="relative h-24 w-24 lg:h-36 lg:w-36">
-        <Image fill alt="정보 수정 이미지" src="/icons/check-circle.svg" />
+        <Image
+          fill
+          alt={`${type} 아이콘`}
+          src={iconSrc ?? defaultIcons[type]}
+        />
       </div>
       <span className="flex-1">{message}</span>
     </div>
