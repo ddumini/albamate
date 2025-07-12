965fb540792ce85215fda6ac28e616e7208eb4d0
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 05:38:20 +0900

    💄Style: 팝업 토스트 세부 반응형 조정

--- src/shared/components/common/popup/Popup.tsx
+++ src/shared/components/common/popup/Popup.tsx
@@ -25,16 +25,22 @@ const Popup = ({
   if (!visible) return null;
 
   return (
-    <div className="BG-blueblack Text-white-gray fixed top-50 left-1/2 z-50 flex -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap opacity-95 shadow-lg md:px-80 md:text-md lg:px-160 lg:text-lg">
+    <div
+      className="BG-blueblack Text-white-gray fixed top-50 left-1/2 z-50 mx-12 flex max-w-[1200px] -translate-x-1/2 items-center gap-4 rounded-xl px-24 py-12 text-xs whitespace-nowrap opacity-95 shadow-lg md:px-80 md:text-md lg:px-[300px] lg:text-lg"
+      style={{ width: 'calc(100vw - 3rem * 2)' }} // 3rem = 12 (mx-12)
+    >
+      {/* 아이콘 */}
       <div className="relative h-24 w-24 lg:h-36 lg:w-36">
         <Image fill alt="사용자 이미지" src="/icons/user.svg" />
       </div>
 
+      {/* 메시지 */}
       <span className="flex-1">
         현재 <span className="font-semibold text-mint-400">{count}명</span>이 이
         알바폼을 보는 중이에요!
       </span>
 
+      {/* 닫기 버튼 */}
       <button
         aria-label="닫기"
         className="relative h-24 w-24 cursor-pointer transition hover:brightness-75 md:h-30 md:w-30 lg:h-36 lg:w-36"
