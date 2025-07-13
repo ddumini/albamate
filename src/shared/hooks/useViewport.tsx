2c58e9afc655d651b83038693f295c2f95a63518
Dong-Han-Kim <dh32110@gmail.com>
2025-07-13 17:37:46 +0900

    🐛 Fix: useViewport 훅 window 예외처리 추가

--- src/shared/hooks/useViewport.tsx
+++ src/shared/hooks/useViewport.tsx
@@ -5,14 +5,16 @@ const useViewport = () => {
   const [width, setWidth] = useState<number | null>(null);
 
   useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    if (typeof window !== 'undefined') {
+      const handleResize = () => {
+        setWidth(window.innerWidth);
+      };
+      handleResize();
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }
   }, []);
 
   const currentWidth = width ?? 0;
