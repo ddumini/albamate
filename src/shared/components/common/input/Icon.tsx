bdd796a2c9276ccf47387bfed48338dfb95a8018
leetaesik <vlwkvkd@naver.com>
2025-07-14 03:21:24 +0900

    ✨ Feat: jsDoc추가

--- src/shared/components/common/input/Icon.tsx
+++ src/shared/components/common/input/Icon.tsx
@@ -4,14 +4,53 @@ import { MouseEventHandler } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export interface IconProps {
+  /**
+   * 아이콘 이미지의 경로입니다.
+   */
   src: string | StaticImport;
+  /**
+   * 아이콘 이미지의 대체 텍스트입니다.
+   */
   alt: string;
+  /**
+   * next/image의 `sizes` 속성입니다.
+   * 기본값은 `(min-width:64rem) 36px, 24px` 입니다.
+   */
   sizes?: string;
+  /**
+   * 아이콘 이미지의 실제 렌더링 크기를 결정하는 Tailwind CSS 클래스입니다.
+   * 기본값은 'size-24 lg:size-36' 입니다.
+   */
   sizeClass?: string;
+  /**
+   * 아이콘을 감싸는 최상위 컨테이너(`div` 또는 `button`)에 추가로 적용할 Tailwind CSS 클래스입니다.
+   */
   className?: string;
+  /**
+   * 아이콘 클릭 시 실행될 이벤트 핸들러입니다. 이 prop이 제공되면 최상위 컨테이너는 `button` 태그로 렌더링되어 클릭 가능하게 됩니다.
+   */
   onClick?: MouseEventHandler<HTMLButtonElement | HTMLDivElement>;
 }
 
+/**
+ * 아이콘 컴포넌트입니다.
+ * `onClick` prop 유무에 따라 `button` 또는 `div`로 렌더링됩니다.
+ *  기본적으로 24x24, 1024px 이상에서 36x36으로 렌더링 됩니다.
+ *
+ * @example
+ * // 기본 크기의 아이콘
+ * <Icon alt="위치 아이콘" src="/icons/pin-solid.svg" />
+ *
+ * @example
+ * // 클릭 가능한 40x40 의 아이콘
+ * <Icon
+ * alt="메뉴 아이콘"
+ * sizeClass="size-40"
+ * sizes="40px"
+ * src="/icons/menu.svg"
+ * onClick={() => alert('메뉴 클릭')}
+ * />
+ */
 const Icon = ({
   src,
   alt,
--- src/shared/components/common/input/Input.tsx
+++ src/shared/components/common/input/Input.tsx
@@ -11,10 +11,32 @@ export interface InputProps
     InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
   > {
+  /**
+   * 입력 필드의 시각적 스타일을 정의합니다.
+   * 'solid'는 채워진 배경, 'outlined'는 테두리가 있는 형태입니다.
+   * 기본값은 'solid'입니다.
+   */
   variant?: 'solid' | 'outlined';
+  /**
+   * 입력 필드의 유효성 상태를 나타냅니다.
+   * `true`일 경우, `invalid` 스타일이 적용됩니다
+   */
   isInvalid?: boolean;
 }
 
+/**
+ * Input 컴포넌트입니다.
+ * variant와 isInvalid를 통해 스타일을 적용할 수 있습니다.
+ *
+ * @example
+ * // 기본 `solid` 스타일의 입력 필드
+ * <Input placeholder="이름을 입력하세요" />
+ *
+ * @example
+ * // `outlined` 스타일의 입력 필드
+ * <Input variant="outlined" placeholder="이메일을 입력하세요" />
+ *
+ */
 const Input = ({
   variant = 'solid',
   isInvalid,
