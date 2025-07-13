0f2b55e3a3bb266810597fa2901ed39b588ae72d
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-13 23:51:17 +0900

    💄Style: 글로벌 색상으로 커스텀 색상 대체

--- src/shared/components/common/tooltip/TooltipContentWrapper.tsx
+++ src/shared/components/common/tooltip/TooltipContentWrapper.tsx
@@ -1,6 +1,6 @@
 // 기본형과 함수형 툴팁의 공통 스타일 지정
 const TooltipContentWrapper = ({ children }: { children: React.ReactNode }) => (
-  <div className="BG-blueblack Text-white-gray relative z-10 flex items-center gap-2 rounded-lg px-16 py-12 text-xs shadow-lg md:text-md">
+  <div className="Text-white-gray relative z-10 flex items-center gap-2 rounded-lg bg-blue-300 px-16 py-12 text-xs shadow-lg md:text-md">
     {children}
   </div>
 );
