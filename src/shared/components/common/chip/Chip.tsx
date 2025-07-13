b1c86189d8e083de642ce69c40f50ebd07df7e5c
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-14 06:50:07 +0900

    ✨Feat: 반응형 적용 및 varients에 따른 색상 적용

--- src/shared/components/common/chip/Chip.tsx
+++ src/shared/components/common/chip/Chip.tsx
@@ -1,43 +1,40 @@
-// components/Chip.tsx
 'use client';
+
 import { cn } from '@lib/cn';
 import Image from 'next/image';
 
 interface ChipProps {
   label: string;
   variant?: 'default' | 'filled' | 'icon';
-  size?: 'sm' | 'md' | 'lg';
   active?: boolean;
 }
 
 export const Chip = ({
   label,
   variant = 'default',
-  size = 'md',
   active = false,
 }: ChipProps) => {
-  const baseClasses =
-    'rounded-md font-medium border flex items-center gap-1 transition-all';
-  const sizes = {
-    sm: 'text-xs px-2 py-0.5',
-    md: 'text-sm px-3 py-1',
-    lg: 'text-base px-4 py-1.5',
-  };
+  const baseClasses = `
+    rounded-md font-medium border flex justify-center items-center gap-1 transition-all
+    text-xs px-2 py-1 w-12 h-7
+    md:text-sm md:px-10 md:py-4 md:w-56 md:h-32
+    lg:text-base lg:px-12 lg:py-6 lg:w-65 lg:h-38
+  `;
 
   const variants = {
-    default: `bg-white text-gray-500 border-gray-300`,
+    default: 'bg-white text-gray-500 border-gray-300',
     filled: active
-      ? 'bg-teal-700 text-teal-300 border-teal-700'
+      ? 'text-mint-300 bg-mint-50/50 border-mint-300'
       : 'bg-white text-gray-400 border-gray-200',
     icon: 'bg-white text-gray-600 border-gray-300',
   };
 
   return (
-    <span className={cn(baseClasses, sizes[size], variants[variant])}>
+    <span className={cn(baseClasses, variants[variant])}>
       {variant === 'icon' && (
         <Image
           alt="북마크"
-          className="h-4 w-4"
+          className="h-16 w-16"
           height={16}
           src="/icons/bookmark-outlined.svg"
           width={16}
