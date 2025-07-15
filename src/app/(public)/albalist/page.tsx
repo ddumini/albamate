5b40745ec38ecbfb3c569b293c13ad2d7fb5fb24
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-16 05:11:50 +0900

    📦Chore: 폴더구조 확정에 따른 위치 변경

--- src/app/(public)/albalist/page.tsx
+++ src/app/(public)/albalist/page.tsx
@@ -1,11 +1,8 @@
-import MainGnb from '@components/common/gnb/MainGnb';
-
 export default function DashBoardEditPage() {
   return (
     <div>
-      <MainGnb />
       {/* 콘텐츠 영역 */}
-      <div>알바토크</div>
+      <div>알바목록</div>
     </div>
   );
 }
