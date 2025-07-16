fac21fed79c99c9080faeda91c280f88ffb2cb8f
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-16 16:56:31 +0900

    🐛Fix: 함수명 수정

---
+++ src/app/(public)/albatalk/page.tsx
@@ -0,0 +1,13 @@
+import MainGnb from '@components/common/gnb/MainGnb';
+
+const AlbaTalkPage = () => {
+  return (
+    <div>
+      <MainGnb />
+      {/* 콘텐츠 영역 */}
+      <div>알바토크</div>
+    </div>
+  );
+};
+
+export default AlbaTalkPage;
