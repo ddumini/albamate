cfcb1c4d8f0cb83875774c94fa2015f04c428ff3
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-17 21:54:01 +0900

    🐛Fix: 불필요한 import 삭제 및 상하 여백 조절

--- src/shared/components/common/gnb/auth-gnb/Header.tsx
+++ src/shared/components/common/gnb/auth-gnb/Header.tsx
@@ -1,21 +1,15 @@
 'use client';
 
 import { headerWrapper, innerWrapper } from '@common/gnb/styles/Header.styles';
-import { usePathname, useRouter } from 'next/navigation';
 
 import Logo from '@/shared/components/common/gnb/Logo';
 
 import RightMenu from './RightMenu';
 
 const Header = () => {
-  const pathname = usePathname();
-  const router = useRouter();
-
   return (
     <header className={headerWrapper}>
-      <div
-        className={`${innerWrapper} justify-center md:justify-between md:py-3`}
-      >
+      <div className={`${innerWrapper} justify-center md:justify-between`}>
         {/* 좌측 로고 */}
         <div className="flex items-center md:gap-24">
           <Logo />
