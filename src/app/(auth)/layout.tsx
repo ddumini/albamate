b093dbcb8b67a4c8dedc5268accffc21398fea79
sumin <tnals2565@naver.com>
2025-07-18 00:11:06 +0900

    🎨 Style: auth layout padding 수정
    style: auth layout padding 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,7 +8,10 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <AuthProvider>
       <main>
-        <InnerContainer className="flex flex-col gap-48 pt-200" size="sm">
+        <InnerContainer
+          className="flex flex-col gap-48 pt-94 md:pt-130 lg:pt-200"
+          size="sm"
+        >
           <AuthTitleArea />
           {children}
           <AuthForm />
