eb8284caa84d04d5f20253709ad52b51b8a1c3ec
sumin <tnals2565@naver.com>
2025-07-17 16:03:59 +0900

    ✨ Feat: lottie 이미지 사용을 위해 lottie-react 설치
    feat: lottie 이미지 사용을 위해 lottie-react 설치

---
+++ src/shared/components/ui/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import Lottie from 'lottie-react';
+
+const LoadingSpinner = () => {
+  return <Lottie autoplay loop animationData="/animations/loading.lottie" />;
+};
+
+export default LoadingSpinner;
