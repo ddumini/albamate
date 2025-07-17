16a1afa6824e3e19c2a0b937609802a8a66b9fbf
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-17 18:14:44 +0900

    🐛Fix: 폼 만들기 버튼 이동경로 실제 경로로 변경

--- src/features/albalist/components/FloatingFormButton.tsx
+++ src/features/albalist/components/FloatingFormButton.tsx
@@ -5,7 +5,7 @@ import FloatingButtonContainer from '@common/button/FloatingButtonContainer';
 
 const FloatingFormButton = () => (
   <FloatingButtonContainer>
-    <FloatingButton href="/forms/create" text="폼 만들기" type="addForm" />
+    <FloatingButton href="/addform" text="폼 만들기" type="addForm" />
   </FloatingButtonContainer>
 );
 
