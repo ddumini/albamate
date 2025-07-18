20296ceffab802dea6e71460123034a315841e54
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-19 05:38:19 +0900

    ♻️Refactor: 관심사 분리를 위한 타입 분리

--- src/shared/components/common/list/FilterBar.tsx
+++ src/shared/components/common/list/FilterBar.tsx
@@ -8,16 +8,7 @@ import {
   RecruitFilterOptions,
   SortOptions,
 } from '@/shared/constants/filterOptions';
-
-interface FilterBarProps {
-  isOwner: boolean;
-  searchPlaceholder?: string;
-  onInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onIconClick?: () => void;
-  onRecruitFilterChange: (value: string) => void;
-  onPublicFilterChange: (value: string) => void;
-  onSortChange: (value: string) => void;
-}
+import { FilterBarProps } from '@/shared/types/filter';
 
 const FilterBar = ({
   isOwner,
