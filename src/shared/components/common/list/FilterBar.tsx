a4e28d415384707528eb913101363e467ba2c987
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-19 05:37:28 +0900

    ♻️Refactor: 동일하게 사용되는 부분 관심사 분리를 위해 상수화

--- src/shared/components/common/list/FilterBar.tsx
+++ src/shared/components/common/list/FilterBar.tsx
@@ -1,7 +1,13 @@
 'use client';
 
-import IconInput from '@/shared/components/common/input/IconInput';
-import Select from '@/shared/components/common/select';
+import IconInput from '@common/input/IconInput';
+import Select from '@common/select';
+
+import {
+  PublicFilterOptions,
+  RecruitFilterOptions,
+  SortOptions,
+} from '@/shared/constants/filterOptions';
 
 interface FilterBarProps {
   isOwner: boolean;
@@ -22,25 +28,6 @@ const FilterBar = ({
   onPublicFilterChange,
   onSortChange,
 }: FilterBarProps) => {
-  const RecruitFilterOptions = [
-    { value: 'total', label: '전체' },
-    { value: 'recruiting', label: '모집 중' },
-    { value: 'closed', label: '모집 마감' },
-  ];
-
-  const PublicFilterOptions = [
-    { value: 'total', label: '전체' },
-    { value: 'public', label: '공개' },
-    { value: 'private', label: '비공개' },
-  ];
-
-  const sortOptions = [
-    { value: 'latest', label: '최신순' },
-    { value: 'high-wage', label: '시급 높은순' },
-    { value: 'many-applicants', label: '지원자 많은순' },
-    { value: 'many-scraps', label: '스크랩 많은순' },
-  ];
-
   return (
     <div className="w-full py-32 md:px-72 md:py-48 lg:py-60 dark:border-gray-500">
       <div className="mx-auto flex max-w-1479 flex-col gap-20 px-4 md:gap-24 lg:gap-32">
@@ -72,7 +59,7 @@ const FilterBar = ({
             )}
           </div>
           <Select
-            options={sortOptions}
+            options={SortOptions}
             variant="sort"
             wrapperClassName="mt-8"
             onSelect={onSortChange}
