49ca463ca931d3f8aacb878eb1b8fdb7e81597ee
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-19 05:43:41 +0900

    💄Style: filterBar 공용 컴포넌트 사용을 위한 JSDoc 추가

--- src/shared/components/common/list/FilterBar.tsx
+++ src/shared/components/common/list/FilterBar.tsx
@@ -10,6 +10,22 @@ import {
 } from '@/shared/constants/filterOptions';
 import { FilterBarProps } from '@/shared/types/filter';
 
+/**
+ * 필터 바 컴포넌트
+ * - 알바 리스트 페이지, 내 알바폼 등에서 검색, 필터, 정렬 기능을 담당하는 UI를 렌더링합니다.
+ * - 모집 상태 필터, 공개 여부 필터, 정렬 옵션을 포함하며, 검색 입력창과 아이콘 클릭 이벤트를 지원합니다.
+ *
+ * @param {FilterBarProps} props
+ * @param {boolean} props.isOwner - 사장(OWNER) 여부에 따라 공개/비공개 필터를 노출 여부 결정
+ * @param {string} [props.searchPlaceholder] - 검색 입력창에 표시될 플레이스홀더 텍스트
+ * @param {(e: React.ChangeEvent<HTMLInputElement>) => void} [props.onInputChange] - 검색창 입력값 변경 시 호출되는 콜백
+ * @param {() => void} [props.onIconClick] - 검색 아이콘 클릭 시 호출되는 콜백
+ * @param {(value: string) => void} props.onRecruitFilterChange - 모집 상태 필터 변경 시 호출되는 콜백
+ * @param {(value: string) => void} props.onPublicFilterChange - 공개 여부 필터 변경 시 호출되는 콜백
+ * @param {(value: string) => void} props.onSortChange - 정렬 옵션 변경 시 호출되는 콜백
+ *
+ * @returns {JSX.Element} 필터 바 UI를 렌더링하는 React 컴포넌트
+ */
 const FilterBar = ({
   isOwner,
   searchPlaceholder,
@@ -22,6 +38,7 @@ const FilterBar = ({
   return (
     <div className="w-full py-32 md:px-72 md:py-48 lg:py-60 dark:border-gray-500">
       <div className="mx-auto flex max-w-1479 flex-col gap-20 px-4 md:gap-24 lg:gap-32">
+        {/* 검색창 */}
         <IconInput
           alt="검색"
           className="w-327 lg:w-728"
@@ -34,12 +51,14 @@ const FilterBar = ({
         />
         <div className="flex justify-between">
           <div className="flex gap-16">
+            {/* 모집 여부 */}
             <Select
               options={RecruitFilterOptions}
               placeholder="전체"
               variant="filter"
               onSelect={onRecruitFilterChange}
             />
+            {/* 공개 여부 */}
             {isOwner && (
               <Select
                 options={PublicFilterOptions}
@@ -49,6 +68,7 @@ const FilterBar = ({
               />
             )}
           </div>
+          {/* 정렬 */}
           <Select
             options={SortOptions}
             variant="sort"
