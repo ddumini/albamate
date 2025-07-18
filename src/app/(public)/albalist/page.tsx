7014543e3b1309ca4c2149f1c02b79c932ab7371
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-19 03:37:48 +0900

    💄Style: 알바 목록 페이지에 InnerContainer 적용

--- src/app/(public)/albalist/page.tsx
+++ src/app/(public)/albalist/page.tsx
@@ -1,7 +1,16 @@
 import AlbaListPage from '@/features/albalist/components/AlbaListPage';
+import InnerContainer from '@/shared/components/container/InnerContainer';
 
 const Page = () => {
-  return <AlbaListPage />;
+  return (
+    <InnerContainer
+      className="md:max-w-900 lg:max-w-1600"
+      size="lg"
+      tabletSize="md"
+    >
+      <AlbaListPage />
+    </InnerContainer>
+  );
 };
 
 export default Page;
