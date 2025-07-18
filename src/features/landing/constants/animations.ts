54ccabe9f03aad6dc1da5c59feadc3cb75900d6d
sumin <tnals2565@naver.com>
2025-07-18 16:00:55 +0900

    ♻️ Refactor: coderabbit 피드백 상수 컴포넌트 외부로 분리
    refactor: coderabbit 피드백 상수 컴포넌트 외부로 분리

---
+++ src/features/landing/constants/animations.ts
@@ -0,0 +1,41 @@
+/**
+ * @author Landing Team
+ * @since 2025-01-XX
+ * @description 랜딩 페이지 애니메이션 상수
+ * 랜딩 페이지에서 사용되는 애니메이션 설정을 중앙 집중 관리
+ */
+
+export const FLOATING_ANIMATIONS = [
+  {
+    y: [-10, -20, -10],
+    x: [-5, 10, -5],
+    scale: [1, 1.05, 1],
+    rotate: [-2, 3, -2],
+    duration: 3,
+    delay: 0,
+  },
+  {
+    y: [-15, -25, -15],
+    x: [8, -12, 8],
+    scale: [1, 1.08, 1],
+    rotate: [3, -4, 3],
+    duration: 2.5,
+    delay: 0.5,
+  },
+  {
+    y: [-8, -18, -8],
+    x: [-12, 15, -12],
+    scale: [1, 1.03, 1],
+    rotate: [-1, 2, -1],
+    duration: 3.5,
+    delay: 1,
+  },
+  {
+    y: [-12, -22, -12],
+    x: [15, -8, 15],
+    scale: [1, 1.06, 1],
+    rotate: [2, -3, 2],
+    duration: 2.8,
+    delay: 1.5,
+  },
+];
