a357e559382a594ea1580d1593e1558b6e1636e6
sumin <tnals2565@naver.com>
2025-07-18 15:31:12 +0900

    ♻️ Refactor: 신재민 멘토님 피드백 사항 적용
    refactor: 신재민 멘토님 피드백 사항 적용

--- src/features/landing/components/ApplySection.tsx
+++ src/features/landing/components/ApplySection.tsx
@@ -8,44 +8,43 @@ import LandingContainer from './LandingContainer';
 import LandingSection from './LandingSection';
 import SubTitle from './SubTitle';
 
-const subTitles = ['간단한 정보만 입력해도', '알바 지원이 가능합니다'];
-
-const floatingAnimations = [
-  {
-    y: [-10, -20, -10],
-    x: [-5, 10, -5],
-    scale: [1, 1.05, 1],
-    rotate: [-2, 3, -2],
-    duration: 3,
-    delay: 0,
-  },
-  {
-    y: [-15, -25, -15],
-    x: [8, -12, 8],
-    scale: [1, 1.08, 1],
-    rotate: [3, -4, 3],
-    duration: 2.5,
-    delay: 0.5,
-  },
-  {
-    y: [-8, -18, -8],
-    x: [-12, 15, -12],
-    scale: [1, 1.03, 1],
-    rotate: [-1, 2, -1],
-    duration: 3.5,
-    delay: 1,
-  },
-  {
-    y: [-12, -22, -12],
-    x: [15, -8, 15],
-    scale: [1, 1.06, 1],
-    rotate: [2, -3, 2],
-    duration: 2.8,
-    delay: 1.5,
-  },
-];
-
 const ApplySection = () => {
+  const subTitles = ['간단한 정보만 입력해도', '알바 지원이 가능합니다'];
+
+  const floatingAnimations = [
+    {
+      y: [-10, -20, -10],
+      x: [-5, 10, -5],
+      scale: [1, 1.05, 1],
+      rotate: [-2, 3, -2],
+      duration: 3,
+      delay: 0,
+    },
+    {
+      y: [-15, -25, -15],
+      x: [8, -12, 8],
+      scale: [1, 1.08, 1],
+      rotate: [3, -4, 3],
+      duration: 2.5,
+      delay: 0.5,
+    },
+    {
+      y: [-8, -18, -8],
+      x: [-12, 15, -12],
+      scale: [1, 1.03, 1],
+      rotate: [-1, 2, -1],
+      duration: 3.5,
+      delay: 1,
+    },
+    {
+      y: [-12, -22, -12],
+      x: [15, -8, 15],
+      scale: [1, 1.06, 1],
+      rotate: [2, -3, 2],
+      duration: 2.8,
+      delay: 1.5,
+    },
+  ];
   return (
     <LandingSection>
       <motion.div
