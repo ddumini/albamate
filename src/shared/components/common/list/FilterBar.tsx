d84c233ac7a99f37df5b708d24432551f322d7f6
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-19 05:36:11 +0900

    ✨Feat: 다르게 적용되는 부분 외부에서 prop으로 받을 수 있도록 공용화

--- src/shared/components/common/list/FilterBar.tsx
+++ src/shared/components/common/list/FilterBar.tsx
@@ -1,4 +1,3 @@
-// components/common/filter/FilterBar.tsx
 'use client';
 
 import IconInput from '@/shared/components/common/input/IconInput';
@@ -6,30 +5,38 @@ import Select from '@/shared/components/common/select';
 
 interface FilterBarProps {
   isOwner: boolean;
-  onSearch: (keyword: string) => void;
-  onFilterChange: (value: string) => void;
+  searchPlaceholder?: string;
+  onInputChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onIconClick?: () => void;
+  onRecruitFilterChange: (value: string) => void;
+  onPublicFilterChange: (value: string) => void;
   onSortChange: (value: string) => void;
 }
 
 const FilterBar = ({
   isOwner,
-  onSearch,
-  onFilterChange,
+  searchPlaceholder,
+  onInputChange,
+  onIconClick,
+  onRecruitFilterChange,
+  onPublicFilterChange,
   onSortChange,
 }: FilterBarProps) => {
   const RecruitFilterOptions = [
     { value: 'total', label: '전체' },
     { value: 'recruiting', label: '모집 중' },
-    { value: 'oldest', label: '모집 마감' },
+    { value: 'closed', label: '모집 마감' },
   ];
+
   const PublicFilterOptions = [
     { value: 'total', label: '전체' },
     { value: 'public', label: '공개' },
     { value: 'private', label: '비공개' },
   ];
+
   const sortOptions = [
     { value: 'latest', label: '최신순' },
-    { value: 'high-wage', label: '시급높은순' },
+    { value: 'high-wage', label: '시급 높은순' },
     { value: 'many-applicants', label: '지원자 많은순' },
     { value: 'many-scraps', label: '스크랩 많은순' },
   ];
@@ -41,11 +48,11 @@ const FilterBar = ({
           alt="검색"
           className="w-327 lg:w-728"
           iconClassName="pl-16"
-          iconOnClick={() => alert('검색')}
+          iconOnClick={onIconClick}
           inputClassName="rounded-2xl lg:rounded-3xl lg:pl-68"
-          placeholder="어떤 알바를 찾고 계세요?"
+          placeholder={searchPlaceholder}
           src="/icons/search.svg"
-          onChange={e => onSearch?.(e.target.value)}
+          onChange={onInputChange}
         />
         <div className="flex justify-between">
           <div className="flex gap-16">
@@ -53,14 +60,14 @@ const FilterBar = ({
               options={RecruitFilterOptions}
               placeholder="전체"
               variant="filter"
-              onSelect={onFilterChange}
+              onSelect={onRecruitFilterChange}
             />
             {isOwner && (
               <Select
                 options={PublicFilterOptions}
                 placeholder="전체"
                 variant="filter"
-                onSelect={onFilterChange}
+                onSelect={onPublicFilterChange}
               />
             )}
           </div>
