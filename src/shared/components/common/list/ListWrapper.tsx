0363d3ba8c935a722f3d9c81d287c4bbe16f00af
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-19 09:01:28 +0900

    💄Style: 제너릭 리스트 컴포넌트 JSDoc 추가 및 예시 작성

--- src/shared/components/common/list/ListWrapper.tsx
+++ src/shared/components/common/list/ListWrapper.tsx
@@ -11,6 +11,26 @@ interface ListWrapperProps<T extends Identifiable> {
   className?: string;
 }
 
+/**
+ * 제네릭 리스트 컴포넌트로, 다양한 데이터 타입을 받아서 렌더링할 수 있습니다.
+ *
+ * @template T - `id` 속성을 가진 객체 타입
+ * @param {T[]} items - 렌더링할 아이템 배열
+ * @param {(item: T) => React.ReactNode} renderItem - 각 아이템을 렌더링하는 함수
+ * @param {React.ReactNode} [children] - 아이템 리스트 외에 추가적으로 렌더링할 자식 요소
+ * @param {string} [className] - 외부에서 전달할 클래스명
+ * 
+ * 사용 예시
+ * <ListWrapper
+      items={albaList}
+      renderItem={item => (
+        <AlbaCard key={item.id} item={item} />
+      )}
+    >
+      {isOwner && <FloatingFormButton />}
+    </ListWrapper>
+ *
+ */
 const ListWrapper = <T extends Identifiable>({
   items,
   renderItem,
