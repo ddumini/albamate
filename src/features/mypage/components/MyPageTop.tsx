a0d5b3f3b47db78ec8c88dc8f63cff0afe3e84d2
Dong-Han-Kim <dh32110@gmail.com>
2025-07-20 04:04:54 +0900

    🐛 Fix: role의 타입 구체적 명시

--- src/features/mypage/components/MyPageTop.tsx
+++ src/features/mypage/components/MyPageTop.tsx
@@ -1,10 +1,10 @@
 import MyPageTopButtons from './MyPageTopButtons';
 
-interface MypageTopProps {
-  role: string;
+interface MyPageTopProps {
+  role: 'OWNER' | 'APPLICANT';
 }
 
-const MyPageTop = ({ role }: MypageTopProps) => {
+const MyPageTop = ({ role }: MyPageTopProps) => {
   return (
     <section className="flex w-full items-center justify-between py-24">
       <h1 className="text-3xl font-semibold">마이 페이지</h1>
