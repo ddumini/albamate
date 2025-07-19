815facae87e6f81f0d97eede609da5ecae3ec713
hizdev <gpfksdlrn@naver.com>
2025-07-20 05:33:15 +0900

    ✨ Feat: 알바토크 상세페이지 관련 목데이터 추가

---
+++ src/features/albatalk/mocks/mockAlbatalkDetail.ts
@@ -0,0 +1,21 @@
+import { PostDetailResponse } from '../types/albatalk';
+
+const mockAlbatalkDetail: PostDetailResponse = {
+  id: 370,
+  title: 'ㅂㅈㄷㄱ',
+  content: 'ㅂㅈㄷㄱ',
+  imageUrl:
+    'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Albaform/user/414/1752617866179/coplan.png',
+  likeCount: 0,
+  commentCount: 0,
+  createdAt: '2025-07-15T22:17:48.422Z',
+  updatedAt: '2025-07-15T22:17:48.422Z',
+  writer: {
+    id: 414,
+    nickname: '초지동얼음공주',
+    imageUrl: null,
+  },
+  isLiked: false,
+};
+
+export default mockAlbatalkDetail;
