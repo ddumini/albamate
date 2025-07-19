0a8435f1ee14f04206256dfe5a1c2eb275f902b6
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-19 22:21:55 +0900

    🐛Fix: 중복 div 삭제 후 cn 적용

--- src/shared/components/common/list/ListWrapper.tsx
+++ src/shared/components/common/list/ListWrapper.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { cn } from '@/shared/lib/cn'; // className 유틸
+
 interface Identifiable {
   id: number | string;
 }
@@ -19,32 +21,27 @@ interface ListWrapperProps<T extends Identifiable> {
  * @param {(item: T) => React.ReactNode} renderItem - 각 아이템을 렌더링하는 함수
  * @param {React.ReactNode} [children] - 아이템 리스트 외에 추가적으로 렌더링할 자식 요소
  * @param {string} [className] - 외부에서 전달할 클래스명
- * 
- * 사용 예시
- * <ListWrapper
-      items={albaList}
-      renderItem={item => (
-        <AlbaCard key={item.id} item={item} />
-      )}
-    >
-      {isOwner && <FloatingFormButton />}
-    </ListWrapper>
  *
+ * @example
+ * <ListWrapper
+ *   items={albaList}
+ *   renderItem={item => <AlbaCard key={item.id} item={item} />}
+ * >
+ *   {isOwner && <FloatingFormButton />}
+ * </ListWrapper>
  */
 const ListWrapper = <T extends Identifiable>({
   items,
   renderItem,
   children,
-  className = '',
+  className,
 }: ListWrapperProps<T>) => {
   return (
-    <div className={className}>
-      <div className="flex flex-wrap justify-start gap-24">
-        {items.map(item => (
-          <div key={item.id}>{renderItem(item)}</div>
-        ))}
-        {children}
-      </div>
+    <div className={cn('flex flex-wrap justify-start gap-24', className)}>
+      {items.map(item => (
+        <div key={item.id}>{renderItem(item)}</div>
+      ))}
+      {children}
     </div>
   );
 };
