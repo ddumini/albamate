a0a90f112128ac940f5e5174efc94d8f400bfe02
hizdev <gpfksdlrn@naver.com>
2025-07-21 03:01:45 +0900

    🐛 Fix: 인디케이터 클릭 이벤트 추가

--- src/shared/components/common/pagination/Indicator.tsx
+++ src/shared/components/common/pagination/Indicator.tsx
@@ -1,16 +1,19 @@
 interface IndicatorProps {
   current: number;
   total: number;
+  onIndicatorClick?: (index: number) => void;
 }
 
-const Indicator = ({ current, total }: IndicatorProps) => {
+const Indicator = ({ current, total, onIndicatorClick }: IndicatorProps) => {
   return (
     <div className="hidden items-center justify-center gap-22 lg:flex">
       {Array.from({ length: total }, (_, i) => {
         return (
-          <div
+          <button
             key={`${i}`}
             className={`rounded-full ${current === i ? 'h-16 w-16 bg-gray-300/60' : 'h-12 w-12 bg-line-100/60'}`}
+            type="button"
+            onClick={() => onIndicatorClick?.(i)}
           />
         );
       })}
