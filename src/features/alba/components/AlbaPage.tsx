a25bb975b9a0d6f8804c8af51df6a70278eb6b7a
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-21 02:57:06 +0900

    🐛Fix: 토스트 팝업 prop명 변경 및 알바 상세 컴포넌트 props 간결화

--- src/features/alba/components/AlbaPage.tsx
+++ src/features/alba/components/AlbaPage.tsx
@@ -1,17 +1,18 @@
 'use client';
 
+import AlbaDetail from '@alba/AlbaDetail';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import ToastPopup from '@/shared/components/common/popup/ToastPopup';
 
 import { albaMockData } from '../mocks/mockData';
-import AlbaState from './AlbaState';
 
 const AlbaListPage = () => {
   const { formId } = useParams();
 
   const [popupVisible, setPopupVisible] = useState(false);
+
   useEffect(() => {
     // 페이지 진입 시 1회 팝업 띄우기
     setPopupVisible(true);
@@ -20,7 +21,6 @@ const AlbaListPage = () => {
   // formId는 문자열이므로 숫자로 변환해서 비교
   const item = albaMockData.find(alba => alba.id === Number(formId));
 
-  // item이 없을 경우 처리
   if (!item) {
     return (
       <div className="py-40 text-center text-red-500">
@@ -31,26 +31,16 @@ const AlbaListPage = () => {
 
   return (
     <div className="py-40">
-      <div className="mb-4 text-sm text-gray-500">
+      <div className="mb-40 text-sm text-gray-500">
         알바 상세 페이지 - ID: {formId}
       </div>
       <ToastPopup
-        count={5}
+        applyCount={item.applyCount}
         duration={5000}
         visible={popupVisible}
         onClose={() => setPopupVisible(false)}
       />
-      <AlbaState
-        applyCount={item.applyCount}
-        isPublic={item.isPublic}
-        location={item.location}
-        preferred={item.preferred}
-        recruitmentEndDate={item.recruitmentEndDate}
-        recruitmentStartDate={item.recruitmentStartDate}
-        scrapCount={item.scrapCount}
-        storeName={item.storeName}
-        title={item.title}
-      />
+      <AlbaDetail item={item} />
     </div>
   );
 };
