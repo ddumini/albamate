7d389f0c45fe95a02ec98c37c39e0bdb24e30418
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-21 10:19:26 +0900

    💄Style: 버튼 컴포넌트 적용에 따른 padding-bottom 제거

--- src/features/alba/components/AlbaLocation.tsx
+++ src/features/alba/components/AlbaLocation.tsx
@@ -4,7 +4,7 @@ import KakaoMap from '@/shared/components/ui/KakaoMap';
 const AlbaLocation = () => {
   return (
     <div>
-      <section className="mb-80">
+      <section>
         <p className="py-16 text-2lg font-bold lg:text-[26px]">근무 지역</p>
         <CopyAddress location="서울특별시 중구 세종대로 110" />
         <KakaoMap location="서울특별시 중구 세종대로 110" />{' '}
