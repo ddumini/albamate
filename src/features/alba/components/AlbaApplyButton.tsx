253bfc5f1ebdd9571d474a012d4987f274fa8a5e
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-21 11:08:47 +0900

    ✨Feat: 역할에 따른 지원 버튼 컴포넌트 구현

--- src/features/alba/components/AlbaApplyButton.tsx
+++ src/features/alba/components/AlbaApplyButton.tsx
@@ -1,19 +1,27 @@
+/* eslint-disable react/prop-types */
 import PrimaryButton from '@/shared/components/common/button/PrimaryButton';
 
-const AlbaApplyButton = () => {
+interface AlbaApplyButtonProps {
+  myId: number; // 현재 로그인한 사용자 ID
+  ownerId: number; // 알바 글 작성자 ID
+}
+
+const AlbaApplyButton: React.FC<AlbaApplyButtonProps> = ({ myId, ownerId }) => {
+  const isOwner = myId === ownerId;
+
   return (
     <div className="flex flex-col gap-10 py-16">
       <PrimaryButton
         className="flex w-full max-w-640 py-20 text-lg lg:text-xl"
-        iconSrc="/icons/writing.svg"
-        label="지원하기"
+        iconSrc={isOwner ? '/icons/edit.svg' : '/icons/writing.svg'}
+        label={isOwner ? '수정하기' : '지원하기'}
         type="button"
         variant="solid"
       />
       <PrimaryButton
         className="w-full max-w-640 py-20 text-lg lg:text-xl"
-        iconSrc="/icons/apply-list.svg"
-        label="내 지원 내역 보기"
+        iconSrc={isOwner ? '/icons/delete.svg' : '/icons/apply-list.svg'}
+        label={isOwner ? '삭제하기' : '내 지원 내역 보기'}
         type="button"
         variant="outline"
       />
