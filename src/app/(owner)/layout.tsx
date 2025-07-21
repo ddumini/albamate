de01c0c7eb9be399cc18ea49428e4fd5a8960b46
Dong-Han-Kim <dh32110@gmail.com>
2025-07-21 13:44:44 +0900

    🐛 Fix: ModalManager 중복 제거

--- src/app/(owner)/layout.tsx
+++ src/app/(owner)/layout.tsx
@@ -1,12 +1,10 @@
 import MainGnb from '@/shared/components/common/gnb/main-gnb';
-import ModalManager from '@/shared/components/common/modal/ModalManager';
 
 const OwnerLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <MainGnb />
       <main className="mt-52 md:mt-72 lg:mt-84">{children}</main>
-      <ModalManager />
     </>
   );
 };
--- src/app/(owner)/owner-mypage/layout.tsx
+++ src/app/(owner)/owner-mypage/layout.tsx
@@ -1,15 +1,12 @@
 import MainGnb from '@common/gnb/main-gnb';
 import { ReactNode } from 'react';
 
-import ModalManager from '@/shared/components/common/modal/ModalManager';
-
 const MyPageLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <MainGnb />
       <main className="flex justify-center px-24 pt-85 md:px-72">
         {children}
-        <ModalManager />
       </main>
     </>
   );
