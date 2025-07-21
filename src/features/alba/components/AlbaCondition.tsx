3ec6a6959d929242b437fe424a1b00f7cea33a97
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-21 10:06:02 +0900

    ✨Feat: 모집 조건 컴포넌트 UI 구현

--- src/features/alba/components/AlbaCondition.tsx
+++ src/features/alba/components/AlbaCondition.tsx
@@ -7,30 +7,24 @@ interface AlbaConditionProps {
 }
 
 const AlbaCondition: React.FC<AlbaConditionProps> = ({ item }) => {
+  const conditions = [
+    { label: '모집인원', value: `${item.numberOfPositions}명` },
+    { label: '성별', value: item.gender },
+    { label: '학력', value: item.education },
+    { label: '연령', value: `${item.age}세` },
+    { label: '우대사항', value: item.preferred },
+  ];
+
   return (
-    <div>
+    <div className="max-w-640 lg:flex lg:flex-col lg:gap-24">
       <div className="py-16 text-2lg font-bold lg:text-[26px]">모집 조건</div>
-      <div className="flex flex-col gap-2 rounded-lg border border-gray-25 bg-gray-25 px-16 py-10 text-neutral-900">
-        <div className="grid grid-cols-[1fr_3fr]">
-          <span className="text-gray-600">모집인원</span>
-          <span>{item.numberOfPositions}명</span>
-        </div>
-        <div className="grid grid-cols-[1fr_3fr]">
-          <span className="text-gray-600">성별</span>
-          <span>{item.gender}</span>
-        </div>
-        <div className="grid grid-cols-[1fr_3fr]">
-          <span className="text-gray-600">학력</span>
-          <span>{item.education}</span>
-        </div>
-        <div className="grid grid-cols-[1fr_3fr]">
-          <span className="text-gray-600">연령</span>
-          <span>{item.age}세</span>
-        </div>
-        <div className="grid grid-cols-[1fr_3fr]">
-          <span className="text-gray-600">우대사항</span>
-          <span>{item.preferred}</span>
-        </div>
+      <div className="flex flex-col gap-16 rounded-lg border border-gray-50 bg-gray-25 px-16 py-10 text-neutral-900 lg:p-24">
+        {conditions.map(cond => (
+          <div key={cond.label} className="grid grid-cols-[1fr_3fr]">
+            <span className="text-gray-600">{cond.label}</span>
+            <span>{cond.value}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
