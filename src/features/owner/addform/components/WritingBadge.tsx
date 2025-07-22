eadf552be14056e7adbb1d93f741defe0624edb7
leetaesik <vlwkvkd@naver.com>
2025-07-22 23:40:40 +0900

    💄 Style: 작성중 뱃지 다크모드 추가

--- src/features/owner/addform/components/WritingBadge.tsx
+++ src/features/owner/addform/components/WritingBadge.tsx
@@ -6,8 +6,8 @@ const WritingBadge = ({ isActive }: { isActive?: boolean }) => {
       className={cn(
         'flex h-28 w-48 items-center justify-center rounded-full border text-xs font-semibold lg:h-38 lg:w-66 lg:border-2 lg:text-lg lg:font-bold',
         isActive
-          ? 'border-gray-25 bg-mint-200 text-gray-25'
-          : 'border-gray-100 bg-background-100 text-gray-300'
+          ? 'border-gray-25 bg-mint-200 text-gray-25 dark:border-gray-100 dark:bg-mint-350 dark:text-gray-50'
+          : 'border-gray-100 bg-background-100 text-gray-300 dark:border-gray-200 dark:bg-gray-600'
       )}
     >
       작성중
