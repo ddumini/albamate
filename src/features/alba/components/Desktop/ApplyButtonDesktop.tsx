bde29e352c53a306012a9e65570583d889f584f8
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-23 00:27:32 +0900

    ✨Feat: 알바 폼 삭제 모달 및 지원 내역 확인 모달 적용

--- src/features/alba/components/Desktop/ApplyButtonDesktop.tsx
+++ src/features/alba/components/Desktop/ApplyButtonDesktop.tsx
@@ -2,6 +2,10 @@
 import React from 'react';
 
 import PrimaryButton from '@/shared/components/common/button/PrimaryButton';
+import useModalStore from '@/shared/store/useModalStore';
+
+import ApplicationListModal from '../modal/ApplicationListModal';
+import FormDeleteModal from '../modal/FormDeleteModal';
 
 interface AlbaApplyButtonDesktopProps {
   isOwner: boolean;
@@ -10,6 +14,16 @@ interface AlbaApplyButtonDesktopProps {
 const ApplyButtonDesktop: React.FC<AlbaApplyButtonDesktopProps> = ({
   isOwner,
 }) => {
+  const { openModal } = useModalStore();
+
+  const handleOpenApplicationModal = () => {
+    openModal(<ApplicationListModal />);
+  };
+
+  const handleOpenFormDeleteModal = () => {
+    openModal(<FormDeleteModal />);
+  };
+
   return (
     <div className="flex flex-col gap-10 py-16">
       <PrimaryButton
@@ -25,6 +39,9 @@ const ApplyButtonDesktop: React.FC<AlbaApplyButtonDesktopProps> = ({
         label={isOwner ? '삭제하기' : '내 지원 내역 보기'}
         type="button"
         variant="outline"
+        onClick={
+          isOwner ? handleOpenFormDeleteModal : handleOpenApplicationModal
+        }
       />
     </div>
   );
