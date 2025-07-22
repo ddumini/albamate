0137ffdfb5845a69668842f45d016ccb084339ac
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-22 15:13:00 +0900

    ✨Feat: 내 지원 내역 확인 모달 초기 구현

---
+++ src/features/alba/components/modal/ApplicationListModal.tsx
@@ -0,0 +1,13 @@
+// components/modal/ApplicationListModal.tsx
+import React from 'react';
+
+const ApplicationListModal = () => {
+  return (
+    <div className="p-24 text-gray-800">
+      <h2 className="text-xl font-bold">내 지원 내역 확인하기</h2>
+      <p className="mt-8">지원한 항목이 여기에 표시됩니다.</p>
+    </div>
+  );
+};
+
+export default ApplicationListModal;
