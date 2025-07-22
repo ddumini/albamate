1945a4432fd856184db9e45e8a37cc5b69ecbcf5
leetaesik <vlwkvkd@naver.com>
2025-07-22 16:13:32 +0900

    🐛 Fix: dark모드 input 색상 변경

--- src/shared/components/common/input/inputStyles.ts
+++ src/shared/components/common/input/inputStyles.ts
@@ -10,7 +10,7 @@ export const inputStyle = {
 
 export const inputVariants = {
   solid:
-    'bg-background-200 hover:bg-background-300 dark:bg-gray-700 dark:hover:bg-gray-800',
+    'bg-background-200 hover:bg-background-300 dark:bg-gray-800 dark:hover:bg-gray-700',
   outlined:
     'border border-gray-200 hover:border-gray-400 focus:border-mint-300',
 };
