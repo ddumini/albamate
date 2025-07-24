eb1d7aa9e0d772b4a1e1eb337121b3412c4038b1
sumin <tnals2565@naver.com>
2025-07-24 14:47:16 +0900

    ✨ Feat: auth 작업 환경 세팅
    feat: auth 작업 환경 세팅

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,30 @@
+import 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id?: string;
+      email: string;
+      name: string;
+      role?: string;
+    };
+    accessToken?: string;
+  }
+
+  interface User {
+    id: string;
+    email: string;
+    name: string;
+    role?: string;
+    accessToken?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    email: string;
+    name: string;
+    role?: string;
+    accessToken?: string;
+  }
+}
