c82e9972281a566d75c66dbd50ead0b70799b60a
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-07-25 07:18:28 +0900

    💄Style: gap 조절로 요소 잘림 해결

--- src/features/alba/components/Desktop/AlbaPageDesktop.tsx
+++ src/features/alba/components/Desktop/AlbaPageDesktop.tsx
@@ -18,7 +18,7 @@ interface AlbaPageDesktopProps {
 
 const AlbaPageDesktop = ({ item, isOwner }: AlbaPageDesktopProps) => {
   return (
-    <div className="mx-auto grid grid-cols-12 gap-120">
+    <div className="mx-auto grid grid-cols-12 gap-32">
       {/* 왼쪽 열 */}
       <div className="col-span-6 flex flex-col gap-32">
         <AlbaDetail item={item} />
@@ -27,7 +27,7 @@ const AlbaPageDesktop = ({ item, isOwner }: AlbaPageDesktopProps) => {
       </div>
 
       {/* 오른쪽 열 */}
-      <div className="col-span-6 flex flex-col justify-end gap-32">
+      <div className="col-span-6 flex flex-col gap-32">
         <AlbaInfo item={item} />
         <AlbaContact item={item} />
         <ApplyButtonDesktop isOwner={isOwner} itemId={item.id} />
