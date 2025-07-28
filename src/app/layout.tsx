8b7e304a215240758afaddf963b907bd4fb0e5fb
leetaesik <vlwkvkd@naver.com>
2025-07-28 17:21:10 +0900

    🐛 Fix: viewport width 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: '#ffffff',
+  width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
   minimumScale: 1,
