b7526331c4469fd6303ef0a4ec388d356a2e821c
hizdev <gpfksdlrn@naver.com>
2025-07-31 18:27:00 +0900

    🐛 Fix: 목데이터 제거

--- src/app/(public)/albatalk/[id]/page.tsx
+++ src/app/(public)/albatalk/[id]/page.tsx
@@ -1,12 +1,8 @@
 import AlbatalkDetail from '@/features/albatalk/components/albatalk-detail';
-import mockAlbatalkComments from '@/features/albatalk/mocks/mockAlbatalkComments';
-import mockAlbatalkDetail from '@/features/albatalk/mocks/mockAlbatalkDetail';
 
-const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+const Page = async ({ params }: { params: Promise<{ id: number }> }) => {
   const { id } = await params;
-  const albatalkData = mockAlbatalkDetail;
-  const commentsData = mockAlbatalkComments;
-  return <AlbatalkDetail comments={commentsData} data={albatalkData} />;
+  return <AlbatalkDetail albatalkId={id} />;
 };
 
 export default Page;
