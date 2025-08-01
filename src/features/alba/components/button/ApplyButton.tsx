2ac4c0076ccea6a14bde3137f4e498361967cb63
yujin Jeon <101913688+yuj2n@users.noreply.github.com>
2025-08-02 03:36:23 +0900

    ✨Feat: 로그인 여부에 따라 내 지원 내역 보기 버튼 기능 적용

--- src/features/alba/components/button/ApplyButton.tsx
+++ src/features/alba/components/button/ApplyButton.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/navigation';
 
 import useViewport from '@/shared/hooks/useViewport';
+import { useSessionUtils } from '@/shared/lib/auth/use-session-utils';
 import useModalStore from '@/shared/store/useModalStore';
 
 import ApplicationModal from '../modal/ApplicationModal';
@@ -17,17 +18,31 @@ const ApplyButton = ({ isOwner, id }: ApplyButtonProps) => {
   const { openModal } = useModalStore();
   const router = useRouter();
   const { isTablet, isMobile } = useViewport();
+  const { isAuthenticated } = useSessionUtils();
 
   const handleApply = () => router.push(`/apply/${id}`);
   const handleModify = () => router.push(`/addform?formId=${id}`);
-  const handleApplicationModal = () => openModal(<ApplicationModal id={id} />);
+
+  // 지원 내역 보기 클릭 시
+  const handleViewApplication = () => {
+    if (isAuthenticated) {
+      router.push(`/myapply/${id}`);
+    } else {
+      openModal(<ApplicationModal id={id} />);
+    }
+  };
+
   const handleFormDeleteModal = () => openModal(<FormDeleteModal />);
 
   const isSmallScreen = isMobile || isTablet;
 
   return (
     <div
-      className={` ${isSmallScreen ? 'fixed right-0 bottom-0 left-0 z-50 flex flex-row gap-10 bg-white/70 px-16 py-12 backdrop-blur lg:hidden dark:bg-gray-900/70' : 'hidden flex-col gap-10 py-16 lg:flex'} `}
+      className={` ${
+        isSmallScreen
+          ? 'fixed right-0 bottom-0 left-0 z-50 flex flex-row gap-10 bg-white/70 px-16 py-12 backdrop-blur lg:hidden dark:bg-gray-900/70'
+          : 'hidden flex-col gap-10 py-16 lg:flex'
+      } `}
     >
       {isOwner ? (
         <OwnerButtons
@@ -39,7 +54,7 @@ const ApplyButton = ({ isOwner, id }: ApplyButtonProps) => {
         <ApplicantButtons
           isSmall={isSmallScreen}
           onApply={handleApply}
-          onViewApplication={handleApplicationModal}
+          onViewApplication={handleViewApplication}
         />
       )}
     </div>
