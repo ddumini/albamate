6e9746457f810ee763dd5c5e58ed063afab3cd7b
sumin <tnals2565@naver.com>
2025-08-11 19:36:35 +0900

    💄 Style: tab 컴포넌트 다크모드 스타일 수정
    style: tab 컴포넌트 다크모드 스타일 수정

--- src/shared/components/common/tab/Tab.tsx
+++ src/shared/components/common/tab/Tab.tsx
@@ -16,11 +16,11 @@ const Tab = ({ tabs, handleClick }: TabProps) => {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
-    <div className="flex w-fit max-w-327 gap-8 rounded-md bg-gray-25 p-6 text-[14px] md:max-w-422 md:text-[16px]">
+    <div className="flex w-fit max-w-327 gap-8 rounded-md bg-background-200 p-6 text-md md:max-w-422 md:text-lg dark:bg-blue-300/30">
       {tabs.map((item, idx) => (
         <button
           key={item.id}
-          className={`cursor-pointer rounded-md px-23 py-4 md:px-35 md:py-8 ${activeTab === idx ? 'bg-white text-black' : 'bg-white text-gray-400'}`}
+          className={`cursor-pointer rounded-md px-23 py-4 font-medium md:px-35 md:py-8 ${activeTab === idx ? 'bg-white font-semibold text-black-400 dark:bg-blue-300/70 dark:text-gray-100' : 'text-gray-400'}`}
           type="button"
           onClick={() => {
             setActiveTab(idx);
